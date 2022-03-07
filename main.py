6227a7acce70e3ecd394c1bb85eb09cf170d3797
Xol3pa <vang.pva@mail.ru>
2022-03-07 03:40:05 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+import folium
+
+
+def get_info_by_ip(ip='127.0.0.1'):
+    try:
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+        info = {
+            '[IP]': response.get('query'),
+            '[Int prov]': response.get('isp'),
+            '[Org]': response.get('org'),
+            '[Country]': response.get('country'),
+            '[Region Name': response.get('regionName'),
+            '[City]': response.get('city'),
+            '[ZIP]': response.get('zip'),
+            '[Lat]': response.get('lat'),
+            '[Lon]': response.get('lon'),
+        }
+
+        for key, value in info.items():
+            print(f'{key} : {value}')
+
+        area = folium.Map(location=[response.get('lat'), response.get('lon')])
+        area.save(f'{response.get("query")}_{response.get("city")}.html')
+
+    except requests.exceptions.ConnectionError:
+        print(' [!] Please check your connection!')
+
+
+def main():
+    ip = input('Enter correct IP: ')
+
+    get_info_by_ip(ip)
+
+
+if __name__ == '__main__':
+    main()
+
